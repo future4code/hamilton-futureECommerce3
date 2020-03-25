bc6521c1e1bbc6c3ee5be273e2cabe7c20f60ef8
skempfer <shana.kempfer88@gmail.com>
2020-03-25 15:08:12 -0300

    Add filterMinMax.js

---
+++ ecommerce3/src/components/filterMinMax.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from 'prop-types';
+
+const filterMinMax = styled.div`
+
+`;
+export class filterMinMax extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return (
+            <filterMinMax>
+
+            </filterMinMax>
+        );
+    }
+}
+
+export default filterMinMax;
+
